d4a49b0a28f580895cd0109cc01b7673c0f8bfb5
Leon <FeelineKing@gmail.com>
2019-10-31 11:24:54 +0200

    Updates

---
+++ precache-manifest.a2c37788831443a205907bcbbf6e89a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc07f171e37c3d87cf75ed1471154548",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "5c0535da40d50ddfb2a5",
+    "url": "/react_static-list-of-posts/static/css/main.4366667e.chunk.css"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "/react_static-list-of-posts/static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "5c0535da40d50ddfb2a5",
+    "url": "/react_static-list-of-posts/static/js/main.8c9dee09.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
